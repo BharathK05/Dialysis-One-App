f35411d6f8491e1640023a0e3fddb075f2266838
Hari Sharen <harisharen88@gmail.com>
2025-11-10 13:07:12 +0530

    Onboarding commit

---
+++ Dialysis One App/ViewControllers/OnboardingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingViewController.swift
+//  Dialysis One App
+//
+//  Created by user@1 on 10/11/25.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
